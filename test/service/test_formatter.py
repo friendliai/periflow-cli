f20900f1419650a4c941057a19c9d774c4d26bde
kooyunmo <mpbb03@naver.com>
2022-04-23 16:23:47 +0900

    better styling

--- test/service/test_formatter.py
+++ test/service/test_formatter.py
@@ -6,6 +6,7 @@ import pytest
 from rich.table import Table
 
 from pfcli.service.formatter import (
+    PanelFormatter,
     TableFormatter,
     get_value,
 )
@@ -17,9 +18,21 @@ def table_formatter() -> TableFormatter:
         name='Personal Info',
         fields=['required.name', 'email', 'age'],
         headers=['Name', 'Email', 'Age'],
-        extra_fields=['job'],
-        extra_headers=['Occupation'],
+        extra_fields=['job', 'active'],
+        extra_headers=['Occupation', 'Active'],
         caption="This table shows user's personal info"
+    )
+
+
+@pytest.fixture
+def panel_formatter() -> PanelFormatter:
+    return PanelFormatter(
+        name='Personal Info',
+        fields=['required.name', 'email', 'age'],
+        headers=['Name', 'Email', 'Age'],
+        extra_fields=['job', 'active'],
+        extra_headers=['Occupation', 'Active'],
+        subtitle="This table shows user's personal info"
     )
 
 
@@ -41,7 +54,7 @@ def test_get_value():
     assert get_value(data, 'k6') == 'v4'
 
 
-def test_table_formatter(table_formatter: TableFormatter):
+def test_table_formatter(table_formatter: TableFormatter, capsys: pytest.CaptureFixture):
     data = [
         {
             'required': {
@@ -49,7 +62,8 @@ def test_table_formatter(table_formatter: TableFormatter):
             },
             'email': 'koo@friendli.ai',
             'age': 26,
-            'job': 'historian'
+            'job': 'historian',
+            'active': True
         },
         {
             'required': {
@@ -57,9 +71,29 @@ def test_table_formatter(table_formatter: TableFormatter):
             },
             'email': 'kim@friendli.ai',
             'age': 28,
-            'job': 'scientist'
+            'job': 'scientist',
+            'active': False
         }
     ]
     table = table_formatter.get_renderable(data)
     assert isinstance(table, Table)
     table_formatter.render(data)
+    out = capsys.readouterr().out
+    assert 'Active' not in out
+    assert 'Occupation' not in out
+
+    table_formatter.add_substitution_rule('True', 'Yes')
+    table_formatter.add_substitution_rule('False', 'No')
+    table_formatter.apply_styling('Active', style='blue')
+    table = table_formatter.get_renderable(data, show_detail=True)
+    assert isinstance(table, Table)
+    table_formatter.render(data, show_detail=True)
+    out = capsys.readouterr().out
+    assert 'Active' in out
+    assert 'Occupation' in out
+    assert 'Yes' in out
+    assert 'No' in out
+
+
+def test_panel_formatter():
+    ...
