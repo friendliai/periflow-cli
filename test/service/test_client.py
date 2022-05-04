c30f160019db682f5c7c821c142cb7138fda3c0d
SungwooCho <pigbug419@gmail.com>
2022-05-04 15:57:57 +0900

    test

--- test/service/test_client.py
+++ test/service/test_client.py
@@ -2006,4 +2006,62 @@ def test_serve_client_create_serve(requests_mock: requests_mock.Mocker,
 
     requests_mock.post(serve_client.url_template.render(), status_code=404)
     with pytest.raises(typer.Exit):
-        serve_client.create_serve({})    
+        serve_client.create_serve({})
+
+@pytest.mark.usefixtures('patch_auto_token_refresh')
+def test_serve_client_list_serve(requests_mock: requests_mock.Mocker,
+                                 serve_client: ServeClientService):
+    assert isinstance(serve_client, ServeClientService)
+
+    # Success
+    requests_mock.get(
+        serve_client.url_template.render(),
+        json=[
+            {
+                "id": 0,
+                "name": "name",
+                "status": "running",
+                "vm": "aws-g4dn.12xlarge",
+                "gpu_type": "t4",
+                "num_gpus": 1,
+                "start": '2022-04-18T05:55:14.365021Z',
+                "endpoint": "http:0.0.0.0:8000/0/v1/completions"
+            }
+        ]
+    )
+
+    assert serve_client.list_serves() == [
+        {
+            "id": 0,
+            "name": "name",
+            "status": "running",
+            "vm": "aws-g4dn.12xlarge",
+            "gpu_type": "t4",
+            "num_gpus": 1,
+            "start": '2022-04-18T05:55:14.365021Z',
+            "endpoint": "http:0.0.0.0:8000/0/v1/completions"
+        }
+    ]
+
+    requests_mock.get(serve_client.url_template.render(), status_code=404)
+    with pytest.raises(typer.Exit):
+        serve_client.list_serves()
+
+@pytest.mark.usefixtures('patch_auto_token_refresh')
+def test_serve_client_delete_serve(requests_mock: requests_mock.Mocker,
+                                 serve_client: ServeClientService):
+    assert isinstance(serve_client, ServeClientService)
+
+    url_template = deepcopy(serve_client.url_template)
+    url_template.attach_pattern('$serve_id/')
+
+    # Success
+    requests_mock.delete(url_template.render(serve_id=0), status_code=204)
+    try:
+        serve_client.delete_serve(0)
+    except typer.Exit:
+        raise pytest.fail("serve client test failed.")
+    
+    requests_mock.delete(url_template.render(serve_id=0), status_code=404)
+    with pytest.raises(typer.Exit):
+        serve_client.delete_serve(0)
