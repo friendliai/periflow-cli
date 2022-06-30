2e86a3b6044a8cbe9543ab0db879c74851f80082
Gyewon Lee <gyewonlee@friendli.ai>
2022-06-30 08:14:21 +0000

    Unroll parameter dict

--- pfcli/service/client/billing.py
+++ pfcli/service/client/billing.py
@@ -14,14 +14,15 @@ class BillingSummaryClientService(ClientService):
                     day: int = None,
                     group_id: T = None,
                     project_id: T = None) -> List[dict]:
+        params = {
+            "group_id": str(group_id) if group_id else None,
+            "project_id": str(project_id) if project_id else None,
+            "year": year,
+            "month": month,
+            "day": day
+        }
         get_response_dict = safe_request(self.list, err_prefix=f"Failed to get billing summary.")
         return paginated_get(
             get_response_dict,
-            params={
-                "group_id": str(group_id) if group_id else None,
-                "project_id": str(project_id) if project_id else None,
-                "year": year,
-                "month": month,
-                "day": day
-            }
+            **params
         )
