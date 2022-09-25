5eea60d0cc18bba059c3d5fb2049c2af33f9b179
Donghyun Kim <dhkim@friendli.ai>
2022-09-25 17:58:16 +0900

    staging

--- pfcli/utils.py
+++ pfcli/utils.py
@@ -25,14 +25,14 @@ from pfcli.service import CloudType, StorageType, storage_region_map, cloud_regi
 
 # Variables
 periflow_directory = Path.home() / ".periflow"
-periflow_api_server = "https://api-dev.friendli.ai/api/"
-periflow_ws_server = "wss://api-ws-dev.friendli.ai/ws/"
-periflow_discuss_url = "https://discuss-dev.friendli.ai/"
-periflow_mr_server = "https://pfmodelregistry-dev.friendli.ai/"
+periflow_api_server = "https://api-staging.friendli.ai/api/"
+periflow_ws_server = "wss://api-ws-staging.friendli.ai/ws/"
+periflow_discuss_url = "https://discuss-staging.friendli.ai/"
+periflow_mr_server = "https://pfmodelregistry-staging.friendli.ai/"
 periflow_serve_server = "http://0.0.0.0:8000/"
-periflow_auth_server = "https://pfauth-dev.friendli.ai/"
-periflow_meter_server = "https://pfmeter-dev.friendli.ai/"
-periflow_observatory_server = "https://pfo-dev.friendli.ai/"
+periflow_auth_server = "https://pfauth-staging.friendli.ai/"
+periflow_meter_server = "https://pfmeter-staging.friendli.ai/"
+periflow_observatory_server = "https://pfo-staging.friendli.ai/"
 
 
 def get_periflow_directory() -> Path:
