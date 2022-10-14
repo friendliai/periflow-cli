8a0c552fc080c7ac34609cf323fbfc43c8aac564
Taebum Kim <ktaebum@friendli.ai>
2022-10-14 14:26:25 +0900

    billing fix

--- pfcli/billing.py
+++ pfcli/billing.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2021 FriendliAI
 
-from collections import defaultdict
+from datetime import datetime, timedelta
 from functools import reduce
 from typing import Optional
 
@@ -9,10 +9,9 @@ import typer
 
 from pfcli.service import ServiceType
 from pfcli.service.client import build_client
-from pfcli.service.client.billing import BillingClientService
-from pfcli.context import get_current_group_id, get_current_project_id
+from pfcli.service.client.billing import BillingClientService, TimeGranularity
 from pfcli.service.formatter import PanelFormatter, TableFormatter
-from pfcli.utils import secho_error_and_exit
+from pfcli.utils import local_to_utc, utc_to_local
 
 
 tabulate.PRESERVE_WHITESPACE = True
@@ -25,8 +24,8 @@ app = typer.Typer(
 
 table_formatter = TableFormatter(
     name="Billing",
-    fields=["vm_name", "price"],
-    headers=["VM Name", "Amount used in USD"],
+    fields=["start_time", "end_time", "price"],
+    headers=["Start Time", "End Time", "Amount used in USD"],
 )
 
 panel_formatter = PanelFormatter(
@@ -39,41 +38,48 @@ def summary(
     year: int = typer.Argument(...),
     month: int = typer.Argument(...),
     day: Optional[int] = typer.Argument(None),
+    view_project: bool = typer.Option(
+        False, "--project", "-p", help="View project-level cost summary."
+    ),
     view_organization: bool = typer.Option(
-        False, "--organization", "-o", help="View organization-level cost summary"
+        False, "--organization", "-o", help="View organization-level cost summary."
+    ),
+    time_granularity: Optional[TimeGranularity] = typer.Option(
+        None, "--time-granularity", "-t", help="View within the given time granularity."
     ),
 ):
     "Summarize the billing information for the given time range"
     client: BillingClientService = build_client(ServiceType.BILLING_SUMMARY)
-    group_id = None
-    project_id = None
-    if not view_organization:
-        project_id = get_current_project_id()
-        if project_id is None:
-            secho_error_and_exit("Project is not set!")
+
+    agg_by = "user_id"
+
+    if view_project:
+        agg_by = "project_id"
+
+    if view_organization:
+        agg_by = "organization_id"
+
+    if day is None:
+        start_date = datetime(year, month, 1)
+        end_date = datetime(year, (month + 1) % 12, 1) - timedelta(days=1)
     else:
-        group_id = get_current_group_id()
-        if group_id is None:
-            secho_error_and_exit("Organization is not set!")
+        start_date = datetime(year, month, day)
+        end_date = datetime(year, month, day)
 
     prices = client.list_prices(
-        year=year, month=month, day=day, group_id=group_id, project_id=project_id
+        start_date=local_to_utc(start_date).isoformat(),
+        end_date=local_to_utc(end_date).isoformat(),
+        agg_by=agg_by,
+        time_granularity=time_granularity
     )
 
-    price_by_vmname = defaultdict(float)
-
+    total_price = 0
     for price_info in prices:
-        attributes = price_info["instance"]["attributes"]
-        if "vm_name" in attributes:
-            price_by_vmname[attributes["vm_name"]] += price_info["price"]
-    table_formatter.render(
-        [
-            {"vm_name": vm_name, "price": round(price, 2)}
-            for vm_name, price in price_by_vmname.items()
-        ]
-    )
+        price_info["start_time"] = utc_to_local(datetime.strptime(price_info["start_time"], "%Y-%m-%dT%H:%M:%SZ"))
+        price_info["price"] = round(
+            reduce(lambda acc, x: acc + x["price"], price_info["price_list"], 0.0), 2
+        )
+        total_price += price_info["price"]
 
-    price_sum = reduce((lambda acc, x: acc + x), price_by_vmname.values(), 0.0)
-
-    total_price = [{"price": round(price_sum, 2)}]
-    panel_formatter.render(total_price)
+    table_formatter.render(prices)
+    panel_formatter.render([{"price": round(total_price, 2)}])
--- test/service/client/test_billing.py
+++ test/service/client/test_billing.py
@@ -1,7 +1,8 @@
 # Copyright (C) 2021 FriendliAI
 
-import uuid
 from copy import deepcopy
+from datetime import datetime, timedelta
+import uuid
 
 import pytest
 import requests_mock
@@ -13,7 +14,7 @@ from pfcli.service.client.billing import BillingClientService
 
 
 @pytest.fixture
-def billing_summary_client() -> BillingClientService:
+def billing_summary_client(user_project_group_context) -> BillingClientService:
     return build_client(ServiceType.BILLING_SUMMARY)
 
 
@@ -25,24 +26,28 @@ def test_billing_summary_client_get_summary(
 
     url_template = deepcopy(billing_summary_client.url_template)
 
+    now = datetime.now()
+    ten_days_after = now + timedelta(days=10)
     # Success
     requests_mock.get(
         url_template.render(),
         json={
             "results": [
                 {
-                    "instance": {"id": "some_id", "attributes": {"vm_name": "vm_1"}},
-                    "price": 1.557,
+                    "start_time": now.isoformat(),
+                    "end_time": ten_days_after.isoformat(),
+                    "price_list": [{"agg_unit": "user_id", "id": "some_id", "price": 1.557}],
                 }
             ],
-            "next_cursor": None,
         },
     )
-    assert billing_summary_client.list_prices(year=2022, month=6, group_id=uuid.uuid4())
+    assert billing_summary_client.list_prices(start_date=now.isoformat(),
+                                              end_date=ten_days_after.isoformat())
 
     # Failed due to HTTP error
     requests_mock.get(url_template.render(), status_code=404)
     with pytest.raises(typer.Exit):
         assert billing_summary_client.list_prices(
-            year=2022, month=6, group_id=uuid.uuid4()
+            start_date=now.isoformat(),
+            end_date=ten_days_after.isoformat()
         )
