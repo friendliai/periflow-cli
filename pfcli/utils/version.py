7683178d0aa4d3745fdb540c8872270ff242a1ef
kooyunmo <yunmorning@friendli.ai>
2022-12-16 14:24:27 +0900

    support 3.10

--- pfcli/utils/version.py
+++ pfcli/utils/version.py
@@ -1,8 +1,7 @@
 import json
-from typing import Union
 from urllib.request import urlopen
 
-from packaging.version import LegacyVersion, Version, parse as parse_version
+from packaging.version import _BaseVersion, parse as parse_version
 
 
 PERIFLOW_CLI_NAME = "periflow-cli"
@@ -16,7 +15,7 @@ def is_latest_cli_version(ver: str) -> bool:
     return version == latest_version
 
 
-def get_latest_cli_version() -> Union[LegacyVersion, Version]:
+def get_latest_cli_version() -> _BaseVersion:
     pypi_info = json.loads(urlopen(f"{PYPI_BASE_URL}/{PERIFLOW_CLI_NAME}/json").read())
     latest_ver_string = pypi_info["info"]["version"]
     return parse_version(latest_ver_string)
