66495e80d67d4aa015d99540e318ed1b7006ee17
kooyunmo <yunmorning@friendli.ai>
2023-06-02 17:27:53 +0900

    update prod url

--- pfcli/utils/url.py
+++ pfcli/utils/url.py
@@ -6,39 +6,39 @@ from __future__ import annotations
 
 from urllib.parse import urljoin
 
-periflow_api_server = "https://api-staging.friendli.ai/api/"
-periflow_ws_server = "wss://api-ws-staging.friendli.ai/ws/"
-periflow_discuss_url = "https://discuss.friendli.ai/"
-periflow_mr_server = "https://pfmodelregistry-staging.friendli.ai/"
-periflow_serve_server = "https://pfs-staging.friendli.ai/"
-periflow_auth_server = "https://pfauth-staging.friendli.ai/"
-periflow_meter_server = "https://pfmeter-staging.friendli.ai/"
-periflow_observatory_server = "https://pfo-staging.friendli.ai/"
+training_url = "https://training.periflow.ai/api/"
+training_ws_url = "wss://training-ws.periflow.ai/ws/"
+discuss_url = "https://discuss.friendli.ai/"
+registry_url = "https://modelregistry.periflow.ai/"
+serving_url = "https://serving.periflow.ai/"
+auth_url = "https://auth.periflow.ai/"
+meter_url = "https://metering.periflow.ai/"
+observatory_url = "https://observatory.periflow.ai/"
 
 
 def get_auth_uri(path: str) -> str:
-    return urljoin(periflow_auth_server, path)
+    return urljoin(auth_url, path)
 
 
 def get_uri(path: str) -> str:
-    return urljoin(periflow_api_server, path)
+    return urljoin(training_url, path)
 
 
 def get_wss_uri(path: str) -> str:
-    return urljoin(periflow_ws_server, path)
+    return urljoin(training_ws_url, path)
 
 
 def get_pfs_uri(path: str) -> str:
-    return urljoin(periflow_serve_server, path)
+    return urljoin(serving_url, path)
 
 
 def get_mr_uri(path: str) -> str:
-    return urljoin(periflow_mr_server, path)
+    return urljoin(registry_url, path)
 
 
 def get_meter_uri(path: str) -> str:
-    return urljoin(periflow_meter_server, path)
+    return urljoin(meter_url, path)
 
 
 def get_observatory_uri(path: str) -> str:
-    return urljoin(periflow_observatory_server, path)
+    return urljoin(observatory_url, path)
